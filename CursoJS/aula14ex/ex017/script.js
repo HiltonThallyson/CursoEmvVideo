e7655ab4a8329e3593a2acfc51df1ca7af5b139e
HiltonThallyson <hilton.thallyson@hotmail.com>
2022-02-26 18:57:23 -0300

    Novas aulas adicionadas

--- CursoJS/aula14ex/ex017/script.js
+++ CursoJS/aula14ex/ex017/script.js
@@ -1,1 +1,17 @@
-alert(`Olá`)
+function gerarTabuada(){
+    let valorInput = document.querySelector('input#txtvalor')
+    let display = document.querySelector('textArea#txttabuada')
+    
+    if (valorInput.value.length == 0){
+        alert('Por favor, digite um número.')
+    }else{
+        let valor = Number(valorInput.value)
+        let res = 0
+        display.innerHTML = ''
+        display.style.width = '100px'
+        for(var i = 1; i <= 10; i++){
+            res = valor * i
+            display.innerHTML += `${valor}x${i}=${res}` + '\n'
+        }
+    }
+}
---
+++ CursoJS/aula15/ambiente.js
@@ -0,0 +1,7 @@
+let num = [5,8,4]
+num[3] = 6 // colocar elemento em uma posição especifica
+num.push(7) // colocar elemento no próximo espaço vazio
+num.length // checar o comprimento
+num.sort() // organizar o vetor em uma ordem crescente
+console.log(`Nosso vetor é o ${num}`)
+
